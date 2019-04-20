d755d731e39e3c5e441876bb25dee358fcb18c6f
liuhongshuo <2624886248@qq.com>
2019-04-20 17:48:48 +0800

    u

--- resources/views/layouts/adminbase.blade.php
+++ resources/views/layouts/adminbase.blade.php
@@ -13,7 +13,7 @@
 
 
 <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">{{session('uname')}}</a>
+    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">{{session('admin')['uname']}}</a>
 
     <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
