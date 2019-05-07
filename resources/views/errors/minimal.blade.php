911f2bec3bdccdacc884ed87b09f2988b3681584
liuhongshuo <2624886248@qq.com>
2019-05-07 18:12:43 +0800

    a

--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -59,14 +59,9 @@
             </div>
 
 
-            <a href="#" class="badge badge-primary">Primary</a>
+            <a href="{{url('costomer/login')}}" class="badge badge-primary">Primary</a>
             <a href="#" class="badge badge-secondary">Secondary</a>
-            <a href="#" class="badge badge-success">Success</a>
-            <a href="#" class="badge badge-danger">Danger</a>
-            <a href="#" class="badge badge-warning">Warning</a>
-            <a href="#" class="badge badge-info">Info</a>
-            <a href="#" class="badge badge-light">Light</a>
-            <a href="#" class="badge badge-dark">Dark</a>
+
 
 
         </div>
