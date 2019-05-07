170d34ba7f2b1eca1ea952567e9e9af52779641a
liuhongshuo <2624886248@qq.com>
2019-05-07 13:51:04 +0800

    a

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -2,9 +2,7 @@
 
 @section('title', __('Forbidden'))
 @section('code', '403')
-<<<<<<< HEAD
+
 @section('message', __($exception->getMessage() .'sdfsdfs' ?: 'Forbidden'))
-=======
-@section('message', __($exception->getMessage() ?: 'Forbidden1'))
->>>>>>> fcdcbb231111521406b5bcdfec2b515642096a19
+
 
