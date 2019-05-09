48dc0f962d50e9aff333fe8b162598f0d67e3e51
liuhongshuo <2624886248@qq.com>
2019-05-09 17:20:35 +0800

    a

---
+++ resources/views/phone/scan/q2.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.phonebase')
+
+@section('style')
+<style>
+    .top {
+        margin: 0;
+        padding: 0;
+        height: 100%;
+        width: 100%;
+        background-color: #0cb658;
+        text-align: center;
+    }
+</style>
+@endsection
+
+@section('content')
+
+    <div class="top">
+
+
+        <div class="text-center">
+
+            <img src="/pics/pt_logo.png" class="rounded" alt="Cinque Terre">
+
+        </div>
+
+    </div>
+
+
+
+    <div class="card">
+
+        <div class="card-body">
+
+
+            <h3>公司信息</h3>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">{{$company->province}}</li>
+                <li class="list-group-item">{{$company->city}}</li>
+                <li class="list-group-item">{{$company->company_code}}</li>
+                <li class="list-group-item">{{$company->company_name}}</li>
+            </ul>
+
+
+            <h3>商品信息</h3>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">{{$product->sy_goods_name }}</li>
+                <li class="list-group-item">{{$product->sy_brand_name}}</li>
+            </ul>
+
+
+
+        </div>
+
+        <div class="card-footer text-muted">
+
+        </div>
+    </div>
+@endsection
+
+
