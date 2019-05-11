feb04ae9b5c5b242fa57a9207c7320ce1737bc51
liuhongshuo <2624886248@qq.com>
2019-05-11 15:40:52 +0800

    fix 增加商品属性

--- app/Http/Controllers/Customer/ProductController.php
+++ app/Http/Controllers/Customer/ProductController.php
@@ -99,30 +99,22 @@ class ProductController extends CustomerBase
 
 
         $product->sy_cgoods = $cate2_title ;
-
         $product->sy_pcate = $data['cate1'] ;
-
         $product->sy_goods_name = $data['sy_goods_name'] ;$product->sy_brand_name = $data['sy_brand_name'] ;
-
         $product->sy_package_unit =  $data['sy_package_unit'] ;
 
         if ($sy_goods_number)
                 $product->sy_goods_number = $sy_goods_number ;
 
-        $product->sy_goods_number = $data['sy_production_date']  ;
+        $product->sy_production_date = $data['sy_production_date']  ;
         $product->sy_uom = $data['sy_uom']  ;
-
         $product->sy_shelf_life = $data['sy_shelf_life']  ;
         $product->sy_uo_shelf_life = $data['sy_uo_shelf_life']  ;
-
         $product->sy_goods_bases = $data['sy_goods_bases']  ;
         $product->sy_goods_desc = $data['sy_goods_desc']  ;
-
-
         $product->sy_lot = $data['sy_lot']  ;
         $product->sy_goods_level = $data['sy_goods_level']  ;
         $product->sy_origin_cd = $data['sy_origin']  ;
-
         $product->sy_origin_title = Area::q_name( $data['sy_origin']  ,'dict_provinces')[0]->name;
         $product->save();
 
@@ -137,13 +129,16 @@ class ProductController extends CustomerBase
         $product =
              SyGoods::retrive_item($id) ;
 
-
-
         $cate1s =FarmProductBiz::cat1_list() ;
 
 
+        $level =  DictGoodsLevel::all();
+
+        $provinces =  DictProvince::all();
+
+
         if ($product)
-        return view('customer.product_edit',compact('product' ,'company' , 'cate1s')) ;
+        return view('customer.product_edit',compact('product' ,'company' , 'cate1s', 'level','provinces')) ;
         else
         {
             session()->flash(
@@ -156,17 +151,35 @@ class ProductController extends CustomerBase
     public function edit_post(Request $request){
         parent::haveto_login() ;
 
-
         $data = $this->validate($request,[
             'goods_id'=>'required|integer|min:1',
             'cate1' => 'required|integer|min:1',
             'cate2' => 'required|integer|min:1',
-            'company_id'=>'required|integer|min:1',
             'sy_goods_name'=>'required',
             'sy_brand_name'=>'required',
             'sy_package_unit'=>'required',
+            'sy_uom'=>'required',
+            'sy_production_date' => 'nullable|date',
+            'sy_goods_number'=>'nullable',
+            'sy_shelf_life' => 'required|integer|min:1',
+            'sy_uo_shelf_life'=>'required',
+            'sy_goods_bases'=>'required',
+            'sy_goods_desc'=>'required',
+            'sy_lot'=>'required',
+            'sy_goods_level'=>'required',
+            'sy_origin'=>'required'
         ]) ;
 
+
+//        $data = $this->validate($request,[
+//            'goods_id'=>'required|integer|min:1',
+//            'cate1' => 'required|integer|min:1',
+//            'cate2' => 'required|integer|min:1',
+//            'company_id'=>'required|integer|min:1',
+//            'sy_goods_name'=>'required',
+//            'sy_brand_name'=>'required',
+//            'sy_package_unit'=>'required',
+//        ]) ;
 
         $company =   parent::get_bind_company() ;
 
@@ -178,6 +191,7 @@ class ProductController extends CustomerBase
         }
 
 
+        $province_title = Area::q_name( $data['sy_origin'] ,'dict_provinces')[0]->name;
         SyGood::where([
             'sy_usc_id'=> $company->company_code,
             'sy_goods_id'=>$data['goods_id']
@@ -187,7 +201,20 @@ class ProductController extends CustomerBase
                 'sy_cate_id' =>$data['cate2'],
                 'sy_cgoods' =>$cate2_title,
                 'sy_goods_name' =>$data['sy_goods_name'],
-                'sy_brand_name' =>$data['sy_brand_name']
+                'sy_goods_number'=>$data['sy_goods_number'],
+                'sy_brand_name' =>$data['sy_brand_name'],
+                'sy_package_unit'=>$data['sy_package_unit'],
+                'sy_uom'=>$data['sy_uom'],
+                'sy_production_date'=>$data['sy_production_date'],
+                'sy_shelf_life'=>$data['sy_shelf_life'],
+                'sy_uo_shelf_life'=>$data['sy_uo_shelf_life'],
+                'sy_goods_bases'=>$data['sy_goods_bases'],
+                'sy_goods_desc'=>$data['sy_goods_desc'],
+                'sy_lot'=>$data['sy_lot'],
+
+                'sy_goods_level'=>$data['sy_goods_level'],
+                'sy_origin_cd'=>$data['sy_origin'],
+                'sy_origin_title'=>$province_title,
             ]);
         return redirect(route('product.list') ) ;
     }
