3362549349e030e4abce9aa8bd6e95dd90eb5907
liuhongshuo <2624886248@qq.com>
2019-05-17 12:02:43 +0800

    fix change route url

--- routes/phone.php
+++ routes/phone.php
@@ -1,7 +1,7 @@
 <?php
-
-
-Route::group(['prefix' => 'phone' , 'namespace'=>'Phone'],function () {
-    Route::get('/s', 'ScanController@q')->name('scan.q');
-
-});
+Route::get('/s', 'Phone\ScanController@q')->name('scan.q');
+//http://payline.fooddaily.cn/phone/s?t=1&p=8dae6640-51cc-4a8a-875c-5b354cefa2ff
+//Route::group(['prefix' => 'phone' , 'namespace'=>'Phone'],function () {
+//    Route::get('/s', 'ScanController@q')->name('scan.q');
+//
+//});
