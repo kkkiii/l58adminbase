f49a40e01fa9722ecb4586dc1023bfcfd3a39813
liuhongshuo <2624886248@qq.com>
2019-05-24 18:06:04 +0800

    a

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 Route::get('/home', 'Admin\IndexController@home')->name('home');
-//Route::get('/', 'Admin\LoginController@login');
+Route::get('/', 'Admin\LoginController@login');
 Route::get('/login', 'Admin\LoginController@login')->name('login');
 Route::get('/logout', 'Admin\LoginController@logout')->name('logout');
 Route::post('login', 'Admin\LoginController@store')->name('login.store');
