755d94a91749c24d8beb8ffa14ca3b9550146718
liuhongshuo <2624886248@qq.com>
2019-05-27 14:33:27 +0800

    new 模板处理逻辑

---
+++ app/Biz/TemplateBiz.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 7
+ * Date: 2019/4/14
+ * Time: 17:12
+ */
+
+namespace App\Biz;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB ;
+use App\My\Helpers ;
+use App\My\Category ;
+use App\Model\Org as OrgModel ;
+class TemplateBiz
+{
+
+    static public function qlist($company_code)
+    {
+        $sql = <<<EOD
+SELECT
+	code_views.id,
+	code_views.title,
+	code_views.content,
+	code_views.updated_at,
+	code_views.created_at,
+	company_user_template.company_user_id
+FROM
+	code_views
+INNER JOIN company_user_template ON company_user_template.template_id = code_views.id
+WHERE
+	company_user_id = $company_code
+EOD;
+
+        $res = DB::connection()
+            ->select($sql);
+
+      return $res ;
+
+    }
+    static public function qitem($templateid)
+    {
+        $sql = <<<EOD
+SELECT
+code_views.id,
+code_views.title,
+code_views.content,
+code_views.updated_at,
+code_views.created_at
+FROM
+code_views
+WHERE
+id = $templateid
+EOD;
+
+        $res = DB::connection()
+            ->select($sql);
+        $res =  collect($res)->first();
+
+        return $res ;
+
+    }
+
+}
