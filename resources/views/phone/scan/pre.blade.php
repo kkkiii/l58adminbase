9ef5e5f7a0c67964b384de34c92c950e630bb922
liuhongshuo <2624886248@qq.com>
2019-05-27 14:34:39 +0800

    new: 预览模板

---
+++ resources/views/phone/scan/pre.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.phonebase')
+
+@section('style')
+    <style>
+        .top {
+            margin: 0;
+            padding: 0;
+            height: 100%;
+            width: 100%;
+            background-color: #0cb658;
+            text-align: center;
+        }
+        img{
+            align; center
+        width: auto;
+            height: auto;
+            max-width: 50%;
+            max-height: 50%;
+
+        }
+    </style>
+@endsection
+
+@section('content')
+
+
+
+
+
+    <div class="card">
+
+        <div class="text-center card-body" style="background-color: #17b651">
+
+            <img src="/pics/pt_logo.png" class="img-responsive img-circle" alt="Cinque Terre">
+
+        </div>
+    </div>
+
+
+
+
+    <div class="card">
+
+        <div class="card-body">
+
+
+            {!! $template->content !!}
+
+
+
+
+        </div>
+
+        <div class="card-footer text-muted">
+
+        </div>
+    </div>
+
+
+@endsection
+
+
