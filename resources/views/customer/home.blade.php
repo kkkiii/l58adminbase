524b66788b70cb88b5d9ba757b49d42b1c60f974
liuhongshuo <2624886248@qq.com>
2019-05-27 15:06:02 +0800

    fix文字

--- resources/views/customer/home.blade.php
+++ resources/views/customer/home.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.customerbase')
 
 @section('content')
-    {{ Str::orderedUuid()}}
+    <h1 class="mt-5">操作流程简述</h1>
+    <p class="lead">建立模板(模板就是扫码后显示的样子)</p>
+    <p class="lead">选择你的模板加入购物车</p>
+    <p class="lead">购物车结算,如果没有收货地址,在用户中心创建收货地址</p>
+    <p class="lead">支付后,等待二维码印刷后快递过来</p>
 @endsection
